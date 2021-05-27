c192e7229a21707d623fdebd5104ceefb711e558
RD-Ricardo <ricardodias169@gmail.com>
2021-05-27 14:55:20 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RetPreenchido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int largura, altura;
+            Console.WriteLine("------------------");
+            Console.WriteLine("Ret-Preenchimento");
+            Console.WriteLine("------------------\n");
+            Console.WriteLine("Tamanho do Retangulo");
+            Console.Write("Largura...: ");
+            largura =  Convert.ToInt32(Console.ReadLine());
+            Console.Write("Altura....: ");
+            altura =  Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            for(int i = 0; i< largura; i++)
+            {
+                Console.Write(" # ");
+                for(int c = 1;  c< altura; c++)
+                {
+                    Console.Write(" # ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
